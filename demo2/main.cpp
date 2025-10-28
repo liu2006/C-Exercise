26f0e40abfa0883c788122fe5816b5998ec5a82d
liu2006 <2102033535@qq.com>
2025-10-28 15:25:38 +0800

    commit

--- demo2/main.cpp
+++ demo2/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
+void static_local_example();
+
+void static_local_example() {
+    static size_t interge{1000};
+    std::cout << interge << std::endl;
+    interge += 1000;
+    std::cout << interge << std::endl;
+}
 int main() {
 
-    std::cout << std::endl;
+    static_local_example();
+    static_local_example();
+    // 1000
+    // 2000
+    // 2000
+    // 3000
 
     return 0;
 }
